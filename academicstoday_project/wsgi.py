673e01333d8f3cdc50b02f6a4c5997df6bc37dfe
EnockOMONDI <enockomondike@gmail.com>
2019-11-08 16:57:23 +0300

    added demo project

--- academicstoday_project/wsgi.py
+++ academicstoday_project/wsgi.py
@@ -9,11 +9,14 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
+
+
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "academicstoday_project.settings")
+
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
