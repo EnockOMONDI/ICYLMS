53c68f23ffb151878610b93d0c94dabd187f4f2a
EnockOMONDI <enockomondike@gmail.com>
2020-01-22 20:25:26 +0300

    [#170435157 REFACTOR UI]

--- academicstoday_project/secret_settings.py
+++ academicstoday_project/secret_settings.py
@@ -3,8 +3,8 @@ import sys
 #---------------------------------------------------------------------------#
 # Generic                                                                   #
 #---------------------------------------------------------------------------#
-# SECRET_DEBUG = True
-# SECRET_KEY = 'xd#vc@mec1c0+wz^y&_i^-og&oy$mkn%_yky&xe^fo()mio$up'
+SECRET_DEBUG = True
+SECRET_KEY = 'xd#vc@mec1c0+wz^y&_i^-og&oy$mkn%_yky&xe^fo()mio$up'
 # SECRET_ALLOWED_HOSTS = ['*']
 
 
@@ -31,5 +31,5 @@ SECRET_EMAIL_HOST_PASSWORD = ''
 if 'test' in sys.argv:
     APPLICATION_HAS_ADVERTISMENT = False  # (DO NOT MODIFY)
 else:
-    APPLICATION_HAS_ADVERTISMENT = True  # (True = Yes I want advertisments)
+    APPLICATION_HAS_ADVERTISMENT = False  # (True = Yes I want advertisments)
 APPLICATION_HAS_PUBLIC_ACCESS_TO_TEACHERS = True
