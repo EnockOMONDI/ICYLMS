641180f3083c3fa678d32ed1f5b38314e5c98333
EnockOMONDI <enockomondike@gmail.com>
2020-03-19 06:37:25 +0300

    [#171872559 Refactor UI]

--- static/js/bootstrap/3.3.2/js/bootstrap.js
+++ static/js/bootstrap/3.3.2/js/bootstrap.js
@@ -322,7 +322,7 @@ if (typeof jQuery === 'undefined') {
 
   Carousel.VERSION  = '3.3.2'
 
-  Carousel.TRANSITION_DURATION = 600
+  Carousel.TRANSITION_DURATION = 3000
 
   Carousel.DEFAULTS = {
     interval: 5000,
