d888323aa87017934c42bba92e20012e0f81428d
EnockOMONDI <enockomondike@gmail.com>
2020-08-03 03:40:49 +0300

    #5000 [working course lecture pages]

--- student/views/credit.py
+++ student/views/credit.py
@@ -31,8 +31,8 @@ def credit_page(request, course_id):
         'user' : request.user,
         'tab' : 'credit',
         'HAS_ADVERTISMENT': settings.APPLICATION_HAS_ADVERTISMENT,
-        'local_css_urls' : settings.SB_ADMIN_2_CSS_LIBRARY_URLS,
-        'local_js_urls' : settings.SB_ADMIN_2_JS_LIBRARY_URLS,
+        'local_css_urls' : settings.SB_ADMIN_COURSE_DETAIL_CSS_LIBRARY_URLS,
+        'local_js_urls' : settings.SB_ADMIN_COURSE_DETAIL_JS_LIBRARY_URLS,
     })
 
 
