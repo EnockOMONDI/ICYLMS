bd471ba3aab11c27d0d0b26c27cb2df0ae7b3fb6
EnockOMONDI <enockomondike@gmail.com>
2020-08-07 04:15:31 +0300

    #40090  [update profile msgs transcripts and certs]

--- account/views/profile.py
+++ account/views/profile.py
@@ -16,6 +16,91 @@ from registrar.forms import CourseForm
 from django.contrib import messages
 from django.utils.translation import gettext as _
 from registrar.forms import CourseForm
+from registrar.models import CourseFinalMark
+import datetime
+from account.models import PrivateMessage
+from account.forms import PrivateMessageForm
+
+@login_required(login_url='/landpage')
+def mail_page(request):
+    try:
+        email = request.user.email
+        messages = PrivateMessage.objects.filter(to_address=email)
+    except PrivateMessage.DoesNotExist:
+        messages = None
+    
+    return render(request, 'account/mail/view.html',{
+        'messages': messages,
+        'user': request.user,
+        'tab': 'inbox',
+        'local_css_urls': settings.SB_ADMIN_2_CSS_LIBRARY_URLS,
+        'local_js_urls': settings.SB_ADMIN_2_JS_LIBRARY_URLS,
+    })
+
+
+@login_required()
+def send_private_message(request):
+    response_data = {'status' : 'failed', 'message' : 'unknown error'}
+    if request.is_ajax():
+        if request.method == 'POST':
+            title = request.POST['title']
+            text = request.POST['message']
+            email = request.POST['email']
+            
+            # Validate email exists on system.
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                response_data = {'status' : 'failure', 'message' : 'email does not exist in our system'}
+                return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+            PrivateMessage.objects.create(
+                title=title,
+                text=text,
+                to_address=email,
+                from_address=request.user.email,
+            ).save()
+
+            response_data = {'status' : 'success', 'message' : 'private message sent'}
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+@login_required()
+def view_private_message(request):
+    response_data = {'status' : 'failed', 'message' : 'unknown error'}
+    if request.is_ajax():
+        if request.method == 'POST':
+            message_id = int(request.POST['message_id'])
+            try:
+                message = PrivateMessage.objects.get(id=message_id)
+            except PrivateMessage.DoesNotExist:
+                message = None
+            
+            form = PrivateMessageForm(instance=message)
+            return render(request, 'account/mail/modal.html',{
+                'form': form,
+                'user': request.user,
+                'tab': 'inbox',
+                'local_css_urls': settings.SB_ADMIN_2_CSS_LIBRARY_URLS,
+                'local_js_urls': settings.SB_ADMIN_2_JS_LIBRARY_URLS,
+            })
+
+
+@login_required()
+def delete_private_message(request):
+    response_data = {'status' : 'failed', 'message' : 'unknown error'}
+    if request.is_ajax():
+        if request.method == 'POST':
+            message_id = int(request.POST['message_id'])
+            try:
+                message = PrivateMessage.objects.get(
+                    id=message_id,
+                ).delete()
+            except PrivateMessage.DoesNotExist:
+                message = None
+            
+            response_data = {'status' : 'success', 'message' : 'deleted private message'}
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 
 
@@ -118,12 +203,17 @@ def view_profile(request):
         courses = Course.objects.filter(students__student_id=student.student_id)
     except Course.DoesNotExist:
         courses = None
+    try:
+        marks = CourseFinalMark.objects.filter(student=student)
+    except CourseFinalMark.DoesNotExist:
+        marks = None
         
   
     return render(request, 'account/productionpages/pages/profile2.html',{
    'courses': courses,
    'user' : request.user,    
    'student':student,
+   'marks': marks,
    'local_css_urls' : settings.SB_ADMIN_COURSE_DETAIL_CSS_LIBRARY_URLS,
    'local_js_urls' : settings.SB_ADMIN_COURSE_DETAIL_JS_LIBRARY_URLS,
    }) 
@@ -153,3 +243,60 @@ def trainingbits(request):
                            "js2/buttons.js",
                            "js2/demo.js",
                            "js2/material-kit.min1036.js?v=2.1.1" ],})
+
+@login_required(login_url='/landpageprod')
+def change_certificate_accessiblity(request):
+    response_data = {'status' : 'failed', 'message' : 'unknown error with saving'}
+    if request.is_ajax():
+        if request.method == 'POST':
+            credit_id = int(request.POST['credit_id'])
+            try:
+                mark = CourseFinalMark.objects.get(credit_id=credit_id)
+                if mark.is_public:
+                    mark.is_public = False
+                else:
+                    mark.is_public = True
+                mark.save()
+                response_data = {'status' : 'success', 'message' : 'certificate accessiblity changed'}
+            except CourseFinalMark.DoesNotExist:
+                response_data = {'status' : 'failure', 'message' : 'certificate does not exist'}
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+@login_required()
+def certificate_page(request, credit_id):
+    # Create our student account which will build our registration around.
+    try:
+        student = Student.objects.get(user=request.user)
+    except Student.DoesNotExist:
+        student = Student.objects.create(user=request.user)
+    try:
+        mark = CourseFinalMark.objects.get(credit_id=credit_id)
+    except CourseFinalMark.DoesNotExist:
+        mark = None
+
+    return render(request, 'registrar/certificate/certificate.html',{
+        'student': student,
+        'mark': mark,
+        'user': request.user,
+        'tab': 'certificates',
+        'HAS_ADVERTISMENT': settings.APPLICATION_HAS_ADVERTISMENT,
+        'local_css_urls': settings.SB_ADMIN_2_CSS_LIBRARY_URLS,
+        'local_js_urls': settings.SB_ADMIN_2_JS_LIBRARY_URLS
+    })
+
+
+@login_required()
+def certificate_permalink_modal(request):
+    credit_id = int(request.POST['credit_id'])
+    try:
+        mark = CourseFinalMark.objects.get(credit_id=credit_id)
+    except CourseFinalMark.DoesNotExist:
+        mark = None
+    return render(request, 'registrar/certificate/permalink_modal.html',{
+        'mark': mark,
+        'user': request.user,
+        'tab': 'certificates',
+        'local_css_urls': settings.SB_ADMIN_2_CSS_LIBRARY_URLS,
+        'local_js_urls': settings.SB_ADMIN_2_JS_LIBRARY_URLS
+    })
